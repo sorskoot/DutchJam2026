87b6434ebd0272f684c84c932007da3cdccf1fc3
Sorskoot <metal_c@hotmail.com>
2026-05-02 11:23:28 +0200

    chore(vite.config): update build configuration options

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,21 @@ import {defineConfig} from 'vite';
 
 export default () => {
     return defineConfig({
-        server:  {
+        server: {
             port: 4623,
             host: '0.0.0.0',
+        },
+        build: {
+            target: 'esnext',
+            modulePreload: false,
+            cssCodeSplit: false,
+            assetsInlineLimit: 0,
+            chunkSizeWarningLimit: 10 * 1024 * 1024,
+            rolldownOptions: {
+                output: {
+                    codeSplitting: false,
+                },
+            },
         },
     });
 };
